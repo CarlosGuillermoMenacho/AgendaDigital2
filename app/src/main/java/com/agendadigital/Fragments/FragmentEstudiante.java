470368fae2f16a219380625eebfb53e89d5368ae
Cristhian <cmm14012000@gmail.com>
2020-09-18 11:32:00 -0400

    Esto es una prueba

---
+++ app/src/main/java/com/agendadigital/Fragments/FragmentEstudiante.java
@@ -0,0 +1,26 @@
+package com.agendadigital.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.agendadigital.R;
+
+public class FragmentEstudiante extends Fragment {
+
+
+    public FragmentEstudiante() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_estudiante, container, false);
+    }
+}
